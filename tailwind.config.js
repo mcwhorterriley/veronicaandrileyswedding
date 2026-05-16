d6d60c7ceac658053634790cfbc052fb75142096
Riley McWhorter <rmcwhorter94@gmail.com>
2026-05-16 15:42:49 -0500

    testing changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
+  extend: {
+    colors: {
+      honeybrown: '#6B4A25',
+      caramel: '#85613C',
+      goldaccent: '#C79C53',
+      amberhover: '#A67836',
+      buttercream: '#FDF8F1',
+    },
+  },
   plugins: [],
 }
