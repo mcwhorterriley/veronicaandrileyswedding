acb1986efc9d9fece71b69490bd0aed8ac87b98e
Riley McWhorter <rmcwhorter94@gmail.com>
2026-05-16 19:24:10 -0500

    footer

--- src/WeddingWebsite.jsx
+++ src/WeddingWebsite.jsx
@@ -6,7 +6,7 @@ const ASSETS = {
   background: "/background.png",
   pooh: "/pooh-silhouette.png",
   envelope: "/envelope.png",
-  footer: "/footer.PNG",
+  footer: "/footer.png",
   chime: "/magic-chime.mp3",
   amazon: "/amazon.png",
 
